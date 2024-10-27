f8b9e71c544a2f42097ff9508aa41c47f72b1306
James Freitas <jamesfrj@yahoo.com.br>
2024-10-26 23:09:37 -0300

    feat: :sparkles: Create project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/james-freitas/fc-golang-sum
+
+go 1.23.2
---
+++ sum_test.go
@@ -0,0 +1,37 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+
+	result := sum(2, 3)
+
+	if result != 5 {
+		t.Error("The result must be 5")
+	}
+
+}
+
+func TestSub(t *testing.T) {
+	result := sub(3, 2)
+
+	if result != 1 {
+		t.Error("The result must be 1")
+	}
+}
+
+func TestTimes(t *testing.T) {
+	result := times(2, 2)
+
+	if result != 4 {
+		t.Error("The result must be 4")
+	}
+}
+
+func TestSumX(t *testing.T) {
+	result := sumX(3, 4)
+
+	if result != 10 {
+		t.Error("The result must be 10")
+	}
+}
