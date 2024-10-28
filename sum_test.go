9505cb7d85b983f7a699aee845795d44bdef977e
James Freitas <jamesfrj@yahoo.com.br>
2024-10-28 07:00:19 -0300

    ci: Test sonar removing tests

--- sum_test.go
+++ sum_test.go
@@ -2,36 +2,36 @@ package main
 
 import "testing"
 
-func TestSum(t *testing.T) {
+// func TestSum(t *testing.T) {
 
-	result := sum(2, 3)
+// 	result := sum(2, 3)
 
-	if result != 5 {
-		t.Error("The result must be 5")
-	}
+// 	if result != 5 {
+// 		t.Error("The result must be 5")
+// 	}
 
-}
+// }
 
-func TestSub(t *testing.T) {
-	result := sub(3, 2)
+// func TestSub(t *testing.T) {
+// 	result := sub(3, 2)
 
-	if result != 1 {
-		t.Error("The result must be 1")
-	}
-}
+// 	if result != 1 {
+// 		t.Error("The result must be 1")
+// 	}
+// }
 
-func TestTimes(t *testing.T) {
-	result := times(2, 2)
+// func TestTimes(t *testing.T) {
+// 	result := times(2, 2)
 
-	if result != 4 {
-		t.Error("The result must be 4.")
-	}
-}
+// 	if result != 4 {
+// 		t.Error("The result must be 4.")
+// 	}
+// }
 
-func TestSumX(t *testing.T) {
-	result := sumX(3, 4)
+// func TestSumX(t *testing.T) {
+// 	result := sumX(3, 4)
 
-	if result != 10 {
-		t.Error("The result must be 10")
-	}
-}
+// 	if result != 10 {
+// 		t.Error("The result must be 10")
+// 	}
+// }
